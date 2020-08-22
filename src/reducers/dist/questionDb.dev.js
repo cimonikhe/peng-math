8d3d0e484c0e60b6cba8ba8df6ae2b57074bbe5c
Michael Peterson <michaeljoepeterson@gmail.com>
2020-08-22 16:15:36 -0600

    initial commit

---
+++ src/reducers/dist/questionDb.dev.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var questions = [{
+  title: 'Which of the following is a symbol for a unit of thermodynamic temperature?',
+  options: {
+    a: 'm',
+    b: 'mol',
+    c: 'kg',
+    d: 'cd',
+    e: 'K'
+  },
+  answer: 'e'
+}, {
+  title: 'A mole refers to a quantity of:',
+  options: {
+    a: 'Time',
+    b: 'Amount of substance',
+    c: 'Luminous intensity',
+    d: 'Electric current',
+    e: 'Length'
+  },
+  answer: 'b'
+}, {
+  title: 'The basic unit of mass in the SI system is',
+  options: {
+    a: 'newtons',
+    b: 'cubic metres',
+    c: 'pounds',
+    d: 'kilograms',
+    e: 'cubic centimetres'
+  },
+  answer: 'd'
+}];
