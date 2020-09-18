8a7c7c459dc61c7e9df573b8524be9a5b413bf98
cimonikhe <infocy@yahoo.com>
2020-09-17 19:56:52 -0600

    Most recent update, no errors.

--- src/components/sub-components/question.js
+++ src/components/sub-components/question.js
@@ -8,11 +8,11 @@ function Question(props) {
     <div>  
 
       <p>{props.question.title}</p>
-      <input type="radio" value="A:" name="challenge1" onClick={() => BasicChallenge('A', props.question.answer)()}/> A: {props.question.options.a} <br></br>
-      <input type="radio" value="B:" name="challenge1" onClick={() => BasicChallenge('B', props.question.answer)()}/> B: {props.question.options.b} <br></br>
-      <input type="radio" value="C:" name="challenge1" onClick={() => BasicChallenge('C', props.question.answer)()}/> C: {props.question.options.c} <br></br>
-      <input type="radio" value="D:" name="challenge1" onClick={() => BasicChallenge('D', props.question.answer)()}/> D: {props.question.options.d} <br></br>
-      <input type="radio" value="E:" name="challenge1" onClick={() => BasicChallenge('E', props.question.answer)()}/> E: {props.question.options.e} 
+      <input type="radio" value="A:" name="challenge1" onClick={() => BasicChallenge('A', props.question.answer)}/> A: {props.question.options.a} <br></br>
+      <input type="radio" value="B:" name="challenge1" onClick={() => BasicChallenge('B', props.question.answer)}/> B: {props.question.options.b} <br></br>
+      <input type="radio" value="C:" name="challenge1" onClick={() => BasicChallenge('C', props.question.answer)}/> C: {props.question.options.c} <br></br>
+      <input type="radio" value="D:" name="challenge1" onClick={() => BasicChallenge('D', props.question.answer)}/> D: {props.question.options.d} <br></br>
+      <input type="radio" value="E:" name="challenge1" onClick={() => BasicChallenge('E', props.question.answer)}/> E: {props.question.options.e} 
       <p></p>
       
     </div>
